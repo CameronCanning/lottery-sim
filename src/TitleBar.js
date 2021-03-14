d2156e4b329a07cdbacc16a9a54f8b71899ad391
Cameron Canning <cameroncanning.1@gmail.com>
2021-03-14 18:45:36 -0400

    github button added to title bar

--- src/TitleBar.js
+++ src/TitleBar.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
 import Panel from './Panel';
+import { ReactComponent as GHMark } from './res/gh_mark.svg';
 
 const Title = styled.h2`
     padding: 10px;
-    !#color: ${props => props.theme.primary};
+    margin: 0;
+    margin-right: auto;
+    color: ${props => props.theme.primary};
+`
+const StyledGHMark = styled(GHMark)`
+    width: 100%;
+    height: 100%;
+`
+
+const LogoContainer = styled.div`
+    padding: 10px;
+    width: 24px;
+    height: 24px;
+    margin-top: auto;
+    margin-bottom: auto;
+`
+const StyledPanel = styled(Panel)`
+    display: flex;
+
 `
 export default function TitleBar() {
     return(
-        <Panel>
+        <StyledPanel>
             <Title>Lottery Sim</Title>
-        </Panel>
+                <LogoContainer>
+                    <a href='https://www.github.com/cameroncanning/lottery-sim'>
+                        <StyledGHMark/>
+                    </a>
+                </LogoContainer>                
+        </StyledPanel>
     )
 }
+
+ //<img src={ghMark} alt='GitHub' style={{width: '24px', height: '24px'}}/>
